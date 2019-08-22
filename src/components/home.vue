f9271af517e51c3c2562e69a05181b3c800c5175
好好学习 天天向上 <wanghuiru@192.168.124.17>
2019-08-22 22:22:17 +0800

    aa

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h2>我是home路由</h2>
+      <h2>我是home路由aaaaaaaa</h2>
       <router-link to='/home/news'>新闻</router-link>
       <router-link to='/home/messages'>消息</router-link>
       <router-view></router-view>
